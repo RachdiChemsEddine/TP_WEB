9035aa78f6f8abd650e5e822af164ff222bef963
RachdiChemsEddine <Chems_Eddine.RACHDI@etu.uca.fr>
2023-10-19 09:29:28 +0200

    init repo

---
+++ src/main/java/RACHDI_RAHMANI/TP_WEB/TpWebApplication.java
@@ -0,0 +1,13 @@
+package RACHDI_RAHMANI.TP_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpWebApplication.class, args);
+	}
+
+}
